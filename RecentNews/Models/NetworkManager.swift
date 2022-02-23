bb5828e8dd6f6b1ef075dae44ce5cd770878db15
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-23 17:37:02 -0500

    class NetworkManager created

---
+++ RecentNews/Models/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  RecentNews
+//
+//  Created by Iurie Guzun on 2022-02-23.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        
+    }
+}
