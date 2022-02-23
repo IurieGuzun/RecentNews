f4cab9da6f783176608d13c1c48a94f72610fde2
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-23 17:26:43 -0500

    Initial Commit

---
+++ RecentNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecentNews
+//
+//  Created by Iurie Guzun on 2022-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecentNews/RecentNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecentNewsApp.swift
+//  RecentNews
+//
+//  Created by Iurie Guzun on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct RecentNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
